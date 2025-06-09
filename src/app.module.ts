8cf59f7d62ffc5ab6754c5bc1ecfd504c370d7ad
moises.castro <moises.castro@grupozelo.com>
2025-06-09 16:05:01 -0300

    add validator token

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,6 @@ import { HealthModule } from './health/health.module';
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    //consumer.apply(AuthMiddleware).forRoutes('products');
+    consumer.apply(AuthMiddleware).forRoutes('products');
   }
 }
